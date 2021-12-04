89d5595631bc729d3cf94afee22355489476084c
Charlie Lee <pochanglee2008@gmail.com>
2021-12-04 18:42:08 +0800

    add nonce

--- block.py
+++ block.py
@@ -5,16 +5,18 @@ hash = hashlib.sha256()
 
 
 class Block:
-    def __init__(self, timestamp: int, last_hash: str, hash: str, data: str):
+    def __init__(self, timestamp: int, last_hash: str, hash: str, data: str, nonce: int):
         self.timestamp = timestamp
         self.last_hash = last_hash
         self.hash = hash
         self.data = data
+        self.nonce = nonce
 
     timestamp: int
     last_hash: str
     hash: str
     data: str
+    nonce: int
 
     def __str__(self):
         return '''Block -
@@ -22,31 +24,34 @@ class Block:
         Last Hash: {last_hash}
         Hash: {hash}
         data: {data}
-        '''.format(**vars(self))
+        nonce: {nonce}
+        '''.format(**self.serialize())
 
     @classmethod
     def genesis(cls):
-        return cls(0, '', 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', [])
+        return cls(0, '', 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', [], 0)
 
     @classmethod
     def mine_block(cls, last_block: 'Block', data: str):
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        hash = Block.hash(timestamp=timestamp, last_hash=last_hash, data=data)
-        return cls(timestamp=timestamp, last_hash=last_hash, hash=hash, data=data)
+        nonce = 0
+        hash = Block.hash(timestamp=timestamp,
+                          last_hash=last_hash, data=data, nonce=nonce)
+        return cls(timestamp=timestamp, last_hash=last_hash, hash=hash, data=data, nonce=nonce)
 
     @classmethod
     def hash(cls, **kwargs):
-        data = '{timestamp}{last_hash}{data}'.format(**kwargs)
+        data = '{timestamp}{last_hash}{data}{nonce}'.format(**kwargs)
         m = hashlib.sha256()
         m.update(data.encode())
         return m.hexdigest()
 
     def __eq__(self, block: 'Block'):
-        return self.timestamp == block.timestamp and self.data == block.data and self.hash == block.hash and self.last_hash == block.last_hash
+        return self.serialize() == block.serialize()
 
     def get_hash(self):
-        return Block.hash(timestamp=self.timestamp, last_hash=self.last_hash, data=self.data)
+        return Block.hash(timestamp=self.timestamp, last_hash=self.last_hash, data=self.data, nonce=self.nonce)
 
     def serialize(self) -> dict:
         return dict(
@@ -54,4 +59,5 @@ class Block:
             last_hash=self.last_hash,
             hash=self.hash,
             data=self.data,
+            nonce=self.nonce,
         )
