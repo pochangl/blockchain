6c0f627b2e0b565f27b680af890b95427e559059
Pochang Lee <pochanglee2008@gmail.com>
2021-12-04 14:10:16 +0800

    mine api

--- miner.py
+++ miner.py
@@ -3,7 +3,7 @@ import argparse
 from blockchain import Blockchain
 
 parser = argparse.ArgumentParser(description='Miner process')
-parser.add_argument('port', type=int, help='port number')
+parser.add_argument('port', type=int, default=3000, help='port number')
 
 args = parser.parse_args()
 
@@ -14,8 +14,15 @@ async def get_blocks(request):
     return web.json_response(data=blockchain.serialize())
 
 
+async def post_mine(request: web.Request):
+    json = await request.json()
+    data = json['data']
+    blockchain.add_block(data)
+    return web.HTTPTemporaryRedirect('/blocks')
+
 app = web.Application()
 app.add_routes([web.get('/blocks', get_blocks)])
+app.add_routes([web.post('/mine', post_mine)])
 
 
 web.run_app(app, port=args.port)
